e5e8a33fea6b324952192c5fe4ab0f2272a23c32
Rawradin <edgar.rawr@gmail.com>
2015-12-26 16:04:21 -0800

    bunnies.js

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Beginnings
+#Trying to code the bunny problem/game on http://www.cplusplus.com/articles/N6vU7k9E/
---
+++ bunnies.js
@@ -0,0 +1,9 @@
+//This code will be for the graduation problem on http://www.cplusplus.com/articles/N6vU7k9E/
+
+function bunny(sex,color,age,Name,zombie_bunny){
+  this.sex = sex;
+  this.col = color;
+  this.age = age;
+  this.Name = Name;
+  this.zombie_bunny = zombie_bunny;
+}
